a379733df49ccee5bd782ae10e701735f36816de
David Dunning <dgd59@outlook.com>
2020-06-20 10:31:23 +0100

    Added handling of expected and unexpected HTML errors.

--- favorite/src/app/posts/posts.component.ts
+++ favorite/src/app/posts/posts.component.ts
@@ -1,5 +1,6 @@
 import { PostService } from './../services/post.service';
 import { Component, OnInit } from '@angular/core';
+import { error } from 'protractor';
 
 @Component({
   selector: 'app-posts',
@@ -16,35 +17,64 @@ export class PostsComponent implements OnInit {
   
   ngOnInit(): void {
     this.service.getPosts()
-    .subscribe(response => {
-      this.posts = response.body as any[];
+    .subscribe(
+      response => {
+        this.posts = response.body as any[];
+      },
+      error => {
+        alert('An unexpected error occurred.');
+        console.log(error);
       });
-    }
+  }
 
-    createPost(input: HTMLInputElement){
-      let post = { title: input.value };
-      input.value = '';
-      
-      this.service.postPosts(post)
-      .subscribe(response => {
+  createPost(input: HTMLInputElement){
+    let post = { title: input.value };
+    input.value = '';
+    
+    this.service.postPosts(post)
+    .subscribe(
+      response => {
         post['id'] = (response as any).id;
         this.posts.splice(0, 0, post);
-        });
-    }
+      },
+      (error: Response) => {
+        if (error.status ===400){
+          // report it to the form.setError()
+        }
+        else {
+          alert('An unexpected error occurred.');
+          console.log(error);
+        }
+      });
+  }
 
-    updatePost(post){
-      this.service.patchPosts(post.id, {isRead : true})
-      .subscribe(response => {
+  updatePost(post){
+    this.service.patchPosts(post.id, {isRead : true})
+    .subscribe(
+      response => {
         console.log(response as any);
-        });
-    }
+      },
+      error => {
+        alert('An unexpected error occurred.');
+        console.log(error);
+      });
+  }
 
-    deletePost(post){
-      this.service.deletePosts(post.id)
-      .subscribe(() => {
-        let index = this.posts.indexOf(post)
+  deletePost(post){
+    this.service.deletePosts(345)
+    .subscribe(
+      response => {
+        let index = this.posts.indexOf(post);
         this.posts.splice(index, 1);
-        })
-    }
+      },
+      (error: Response) => {
+        if (error.status === 404)
+          alert("This post has already been deleted.");
+        else {
+          alert('An unexpected error occurred.');
+          console.log(error);
+        }
+      });
+  }
 
 }
