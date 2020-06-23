223d9800c23fecd5379585ce461cb52d5f759b79
David Dunning <dgd59@outlook.com>
2020-06-23 22:49:45 +0100

    A working routing-exercises with just the empty components.

--- routing-execises/src/app/app-routing.module.ts
+++ routing-execises/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ArchiveComponent } from './archive/archive.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'archive/:year/:month', component: ArchiveComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
