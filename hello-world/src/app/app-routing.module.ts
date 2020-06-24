2e84befc9d1e22af1ecd81f263a3795803aca9b1
David Dunning <dgd59@outlook.com>
2020-06-24 09:27:25 +0100

    Moved the hello-world component declarations out of app-routing.module into app.module.

--- hello-world/src/app/app-routing.module.ts
+++ hello-world/src/app/app-routing.module.ts
@@ -1,32 +1,10 @@
-import { NgModule, ErrorHandler } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { AppComponent } from './app.component';
-import { SignupFormComponent } from './signup-form/signup-form.component';
-import { AuthorsComponent } from './authors/authors.component';
-import { FavoriteComponent } from './favorite/favorite.component';
-import { PanelComponent } from './panel/panel.component';
-import { LikeComponent } from './like/like.component';
-import { ZippyComponent } from './zippy/zippy.component';
-import { ContactFormComponent } from './contact-form/contact-form.component';
-import { NewCourseFormComponent } from './new-course-form/new-course-form.component';
-import { ChangePasswordComponent } from './change-password/change-password.component';
 import { PostsComponent } from './posts/posts.component';
 import { GithubFollowersComponent } from './github-followers/github-followers.component';
 import { HomeComponent } from './home/home.component';
 import { GithubProfileComponent } from './github-profile/github-profile.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-import { CoursesComponent } from './courses.component';
-import { SummaryPipe } from './summary.pipe';
-import { InputFormatDirective } from './input-format.directive';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { PostService } from './services/post.service';
-import { CoursesService } from './courses.service';
-import { AuthorsService } from './authors.service';
-import { GithubFollowersService } from './services/github-followers.service';
-import { AppErrorHandler } from './common/app-error-handler';
-import { CourseComponent } from './course/course.component';
-import { HttpClientModule } from '@angular/common/http'
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -37,42 +15,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  declarations: [
-    SignupFormComponent,
-    CourseComponent,
-    CoursesComponent,
-    AuthorsComponent,
-    SummaryPipe,
-    FavoriteComponent,
-    PanelComponent,
-    InputFormatDirective,
-    LikeComponent,
-    ZippyComponent,
-    ContactFormComponent,
-    NewCourseFormComponent,
-    ChangePasswordComponent,
-    PostsComponent,
-    GithubFollowersComponent,
-    HomeComponent,
-    GithubProfileComponent,
-    NotFoundComponent,
-  ],
   imports: [
-    RouterModule.forRoot(routes),
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule
+    RouterModule.forRoot(routes)
   ],
-  exports: [RouterModule],
-  providers: [
-    PostService,
-    CoursesService,
-    AuthorsService,
-    GithubFollowersService,
-    { provide: ErrorHandler, useClass: AppErrorHandler }
-
-  ],
-  bootstrap: [AppComponent]
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
