317405537b4c77ab94f3a8add468c3f431a85681
David Dunning <dgd59@outlook.com>
2020-06-26 20:41:14 +0100

    Added the bounceOutLeft animation using keyframes.

--- animations-demo/src/app/animations.ts
+++ animations-demo/src/app/animations.ts
@@ -1,4 +1,4 @@
-import { transition, trigger, style, animate, state } from '@angular/animations';
+import { transition, trigger, style, animate, state, keyframes } from '@angular/animations';
 
 export let fade = trigger('fade', [
     state('void', style({  opacity: 0 })),
@@ -8,6 +8,28 @@ export let fade = trigger('fade', [
 ]);
 
 export let slide = trigger('slide', [
+    transition(':enter', [
+        style({ transform: "translateX(-20px)"}),
+        animate(1000)
+    ]),
+    // a bounce-out-left animation
+    transition(':leave', [
+        animate('500ms ease-in', keyframes([
+            style({
+                offset: 0.2,
+                opacity: 1,
+                transform: 'translateX(20px)'
+            }),
+            style({
+                offset: 1,
+                opacity: 0,
+                transform: 'translateX(-100%)'
+            })
+        ]))
+    ])
+]);
+
+export let slide1 = trigger('slide-1', [
     transition(':enter', [
         style({ transform: "translateX(-20px)"}),
         animate(1000)
