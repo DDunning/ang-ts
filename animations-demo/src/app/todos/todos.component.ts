7fab50a0583b21898f811b592a42cc964cc58bc4
David Dunning <dgd59@outlook.com>
2020-06-26 17:17:56 +0100

    Working fade animation in the todo component.

--- animations-demo/src/app/todos/todos.component.ts
+++ animations-demo/src/app/todos/todos.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { transition, trigger, style, animate, state } from '@angular/animations';
 
 @Component({
   selector: 'todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css'],
+  animations: [
+    trigger('fade', [
+      state('void', style({  opacity: 0 })),
+      transition('void => *', [
+        animate(2000),
+      ]),
+      transition('* => void', [
+        animate(2000),
+      ])
+    ])
+  ]
 })
 export class TodosComponent {
   items: any[] = [
